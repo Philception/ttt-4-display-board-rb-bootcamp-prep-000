771f15b1d0584f99e390223077041ab38acd3ae2
Phillip Henry III <philtrader71@gmail.com>
2019-11-26 02:14:09 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board_arr)
-  board = [" "," "," "," ","X"," "," "," "," "]
+  board_arr = [" "," "," "," ","X"," "," "," "," "]
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
+
+display_board(board_arr)
